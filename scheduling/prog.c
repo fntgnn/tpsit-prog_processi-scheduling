a804a4d43cdbb36e1c897a875b50f873285433a2
Giovanni Fontana <giovanni@MacBook-Pro-di-Giovanni-2.local>
2016-04-08 18:37:11 +0200

    sjf

--- scheduling/prog.c
+++ scheduling/prog.c
@@ -10,16 +10,37 @@ struct s_proc{
 typedef struct s_proc proc;
 
 void stampa(proc*);
+proc* sjf(proc*);
+int cmpfunc(const void * a, const void * b);
 
 int main(){
-  proc processi[4];
+  proc *processi;
+  processi=(proc*)malloc(N_PROCESSI*sizeof(proc));
   freopen("processi.dat","r",stdin);
   int i=0;
   for(i=0; i<N_PROCESSI; i++){
     scanf("%d,%d,%d\n",&(processi[i].pid),&(processi[i].cpuBurst),&(processi[i].timeArrival));
   }
   stampa(processi);
+  stampa(sjf(processi));
   return 0;
+}
+
+proc* sjf(proc *processi){
+  proc *pnew;//=(proc*)malloc(N_PROCESSI*sizeof(proc));
+  int i=0;
+  int j=0;
+
+  qsort(processi,N_PROCESSI,sizeof(proc),cmpfunc);
+
+  return processi;
+}
+
+int cmpfunc (const void * a, const void * b){
+  const proc *elem1=a;
+  const proc *elem2=b;
+  return (*elem1).cpuBurst - (*elem2).cpuBurst;
+
 }
 
 void stampa(proc *processi){
@@ -28,4 +49,5 @@ void stampa(proc *processi){
   for(i=0;i<N_PROCESSI; i++){
     printf("%d\t%d\t\t%d\n",processi[i].pid,processi[i].cpuBurst,processi[i].timeArrival);
   }
+  printf("\n");
 }
